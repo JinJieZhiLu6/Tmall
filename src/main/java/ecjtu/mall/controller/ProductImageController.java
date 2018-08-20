047e3d75f5589b6168b8ad009bd4d6d963b90276
JinJieZhiLu6 <1539148768@qq.com>
2018-08-20 08:51:30 +0800

    update code

---
+++ src/main/java/ecjtu/mall/controller/ProductImageController.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: ProductImageController
+ * Author:   jack
+ * Date:     18/08/20 8:50
+ * Description: 商品图片管理控制层
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package ecjtu.mall.controller;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈商品图片管理控制层〉
+ *
+ * @author jack
+ * @create 18/08/20
+ * @since 1.0.0
+ */
+public class ProductImageController {
+
+}
